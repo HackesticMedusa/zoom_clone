84949c1706bc1ec9d9c0b6df083309bb54dbdc46
HackesticMedusa <107264491+HackesticMedusa@users.noreply.github.com>
2024-08-18 15:19:01 +0530

    added Reusable Modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,24 +3,29 @@
 import { useState } from "react"
 import HomeCard from "./HomeCard"
 import { useRouter } from "next/navigation"
+import MeetingModal from "./MeetingModal"
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+
+    const createMeeting = () => {
+
+    }
 
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <HomeCard
                 img="/icons/add-meeting.svg"
                 title="New Meeting"
-                description="Start an instant meeting"
+                description="Start an Instant Meeting"
                 className="bg-orange-1"
                 handleClick={() => setMeetingState('isInstantMeeting')}
             />
             <HomeCard
                 img="/icons/schedule.svg"
                 title="Schedule Meeting"
-                description="Plan your meeting"
+                description="Plan your Meeting"
                 className="bg-blue-1"
                 handleClick={() => setMeetingState('isScheduleMeeting')}
             />
@@ -34,9 +39,18 @@ const MeetingTypeList = () => {
             <HomeCard
                 img="/icons/join-meeting.svg"
                 title="Join Meeting"
-                description="via invitation link"
+                description="Via Invitation Link"
                 className="bg-yellow-1"
                 handleClick={() => setMeetingState('isJoiningMeeting')}
+            />
+
+            <MeetingModal 
+                isOpen={meetingState === 'isInstantMeeting'}
+                onClose={() => setMeetingState(undefined)}
+                title="Start an Instant Meeting"
+                className="text-center"
+                buttonText="Start Meeting"
+                handleClick={createMeeting}
             />
         </section>
     )
